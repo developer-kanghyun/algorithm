a6ebbc53835508ea6e68eef92156ef64662bd0f0
kanghyun <dev.kanghyun@gmail.com>
2025-05-02 12:00:00 +0900

    조건별 반환

--- dice.js
+++ dice.js
@@ -1,23 +1,40 @@
-// 머쓱이는 직육면체 모양의 상자를 하나 가지고 있는데 이 상자에 정육면체 모양의 주사위를 최대한 많이 채우고 싶습니다.
-// 상자의 가로, 세로, 높이가 저장되어있는 배열 box와 주사위 모서리의 길이 정수 n이 매개변수로 주어졌을 때,
-// 상자에 들어갈 수 있는 주사위의 최대 개수를 return 하도록 solution 함수를 완성해주세요.
+// 1부터 6까지 숫자가 적힌 주사위가 두 개 있습니다. 두 주사위를 굴렸을 때 나온 숫자를 각각 a, b라고 했을 때 얻는 점수는 다음과 같습니다.
 
-// 1. box의 부피를 구한다
-// 2. 주사위의 부피를 구한다
-// 3. box 안에 들어갈 수 있는 주사위의 최대 갯수를 구한다
-const solution = (box, n) => {
-  return (
-    Math.floor(box[0] / n) * Math.floor(box[1] / n) * Math.floor(box[2] / n)
-  );
+// a와 b가 모두 홀수라면 a2 + b2 점을 얻습니다.
+// a와 b 중 하나만 홀수라면 2 × (a + b) 점을 얻습니다.
+// a와 b 모두 홀수가 아니라면 |a - b| 점을 얻습니다.
+// 두 정수 a와 b가 매개변수로 주어질 때, 얻는 점수를 return 하는 solution 함수를 작성해 주세요.
+
+const solution = (a, b) => {
+  const aIsOdd = a % 2 !== 0;
+  const bIsOdd = b % 2 !== 0;
+
+  if (aIsOdd && bIsOdd) {
+    return a ** 2 + b ** 2;
+  } else if (aIsOdd || bIsOdd) {
+    return 2 * (a + b);
+  } else {
+    return Math.abs(a - b);
+  }
 };
 
-const solution1 = (box, n) => {
-  let [width, length, height] = box;
-
-  return (
-    Math.floor(width / n) * Math.floor(length / n) * Math.floor(height / n)
-  );
+const solution1 = (a, b) => {
+  return a % 2 == 1 && b % 2 == 1
+    ? a ** 2 + b ** 2
+    : a % 2 == 1 || b % 2 == 1
+    ? 2 * (a + b)
+    : Math.abs(a - b);
 };
 
-console.log(solution([1, 1, 1], 1));
-console.log(solution1([1, 1, 1], 1));
+const solution2 = (a, b) => {
+  const isOdd = (num) => num % 2 == 1;
+  return isOdd(a) && isOdd(b)
+    ? a ** 2 + b ** 2
+    : a % 2 == 1 || b % 2 == 1
+    ? 2 * (a + b)
+    : Math.abs(a - b);
+};
+
+console.log(solution(3, 5));
+console.log(solution1(3, 5));
+console.log(solution2(3, 5));
